445920dd9195d2877cc7a3f21afbc76d29e31dd7
Voislav Mishevski <voislav.mishevski@it-labs.com>
2019-10-06 18:00:13 +0200

    Add aws parameter store provider

---
+++ packages/aws-parameter-store/AwsPSProvider.spec.ts
@@ -0,0 +1,14 @@
+import { AwsParameterStoreProvider } from './AwsPSProvider';
+
+describe('AwsParameterStoreProvider', () => {
+  it('should work', () => {
+    const provider = new AwsParameterStoreProvider({
+      transform: {
+        apiUrl: '/ttt/api/url'
+      }
+    });
+    return provider.load().then(() => {
+      expect(provider.data.apiUrl).toEqual('http://localhost:6000/');
+    });
+  });
+});
---
+++ packages/aws-parameter-store/AwsPSProvider.ts
@@ -0,0 +1,38 @@
+import { TransformationSchema } from '@node-config-builder/core/ConfigByTransformLoader';
+import { IConfigurationProvider } from '@node-config-builder/core/models/IConfigurationProvider';
+import { ConfigurationModel } from '@node-config-builder/core/models/ConfigurationModel';
+import { loadConfigByTransformation } from '@node-config-builder/core/ConfigByTransformLoader';
+
+import { SSMClient } from '@aws-sdk/client-ssm-node/SSMClient';
+import { GetParameterCommand } from '@aws-sdk/client-ssm-node';
+
+export interface AwsParameterStoreOptions {
+  transform: TransformationSchema;
+}
+
+export class AwsParameterStoreProvider implements IConfigurationProvider {
+  data: ConfigurationModel;
+
+  private client: SSMClient;
+
+  constructor(private options: AwsParameterStoreOptions) {}
+
+  load(): Promise<void> {
+    this.client = new SSMClient({});
+
+    return loadConfigByTransformation({
+      transform: this.options.transform,
+      getValueByKey: key => this.getByKey(key)
+    }).then(config => {
+      this.data = config;
+    });
+  }
+
+  getByKey(key: string): Promise<string> {
+    return this.client
+      .send(new GetParameterCommand({ Name: key }))
+      .then(res => {
+        return res.Parameter.Value;
+      });
+  }
+}
